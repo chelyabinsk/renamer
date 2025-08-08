a58f34e81ccdef57457f368664212fa9f27fc609
chelyabinsk <15384813+chelyabinsk@users.noreply.github.com>
2025-08-08 22:18:07 +0100

    embed font

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 mod file_ops;
 mod tasks;
 mod ui;
-use iced::{Settings};
+use iced::{Settings, Font};
 use std::borrow::Cow;
 
-const MONO_FONT: &[u8] = include_bytes!("../assets/NotoSans-Regular.ttf");
-
+const MONO_FONT_BYTES: &[u8] = include_bytes!("../assets/NotoSans-Regular.ttf");
+const MONO_FONT_NAME: &str = "Noto Sans";
 
 fn main() -> iced::Result {
     let settings = Settings {
-        fonts: vec![Cow::Borrowed(MONO_FONT)],
+        fonts: vec![Cow::Borrowed(MONO_FONT_BYTES)],
+        default_font: Font::with_name(MONO_FONT_NAME),
         ..Settings::default()
     };
 
